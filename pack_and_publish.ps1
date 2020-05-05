d211c0f2a7b2c4160eef21858383b36b840e683c
Torben Clasen <t.clasen@outlook.com>
2020-05-06 00:14:17 +0200

    add publish script

---
+++ pack_and_publish.ps1
@@ -0,0 +1,15 @@
+param(
+    [string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]$patToken,
+    [string]$shareWith
+)
+
+$path = npm prefix -g
+$tfxcli = Join-Path $path "node_modules\tfx-cli\_build\tfx-cli.js"
+
+Invoke-Expression -Command "node $tfxcli extension create --manifest-globs .\vss_extension.json"
+if ([string]::IsNullOrEmpty($shareWith)) {
+    Invoke-Expression -Command "node $tfxcli extension publish -t $patToken"
+}
+else {
+    Invoke-Expression -Command "node $tfxcli extension publish -t $patToken --share-with $shareWith"
+}
